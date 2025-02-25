a4aaee4228633bc843e85df39596d870ea396a62
Josselin Dionisi <dionisi.josselin@gmail.com>
2025-02-25 16:43:11 +0100

    Fixes

--- index.php
+++ index.php
@@ -17,6 +17,9 @@ foreach ($rooms as $room) {
         case 'Suite':
             $img = 'https://images.unsplash.com/photo-1568495248636-6432b97bd949?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
             break;
+        default:
+            $img = 'uploads/vojtech-bruzek-Yrxr3bsPdS0-unsplash.jpg';
+            break;
     }
 
 
